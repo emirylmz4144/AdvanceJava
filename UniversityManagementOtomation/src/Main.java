12a41b08bcf8bb6cfd92722d56896bb201f88d19
EMİR YILMAZ <emiryilmaz4144@gmail.com>
2022-11-18 18:36:47 +0300

    University Management Otomotion is added

---
+++ UniversityManagementOtomation/src/Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        SecurityMan securityMan=new SecurityMan("Emir Yılmaz","05368824144","emiryilmaz4144@gmail.com","Departman 1",3,"guvenlik.txt");
+        System.out.println(securityMan.spell());
+    }
+}
