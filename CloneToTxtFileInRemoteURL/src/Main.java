373177bc43613e506865169edbdd9c68092278b8
emirylmz4144 <emiryilmaz4144@gmail.com>
2024-03-02 15:52:17 +0300

    CloneToTxtFileInRemoteUrl project updated and readme file added in

--- CloneToTxtFileInRemoteURL/src/Main.java
+++ CloneToTxtFileInRemoteURL/src/Main.java
@@ -7,21 +7,27 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws IOException
     {
-        URL url=new URL("https://www.w3.org/TR/2003/REC-PNG-20031110/iso_8859-1.txt");//içeriği alacağımız url
-        HttpURLConnection connection= (HttpURLConnection) url.openConnection();//url'ye bağlanıyoruz
+        //içeriği alacağımız url ve url'ye bağlanma
+        URL url=new URL("https://www.w3.org/TR/2003/REC-PNG-20031110/iso_8859-1.txt");
+        HttpURLConnection connection= (HttpURLConnection) url.openConnection();
 
-        InputStream inputStream=connection.getInputStream();//Siteden alınan içerik için input stream oluşturuyoruz
+        //Siteden alınan içerik için input stream oluşturuyoruz
+        InputStream inputStream=connection.getInputStream();
 
-        FileOutputStream fileOutputStream=new FileOutputStream("emir.txt");//Bir dosyaya yazdırma için stream oluşturuyoruz
+        //Bir dosyaya yazdırma için stream oluşturuyoruz
+        FileOutputStream fileOutputStream=new FileOutputStream("emir.txt");
 
-        StringBuilder builder=new StringBuilder();//Daha sonra lazım olacak bir string yapıcısı
+        //Daha sonra lazım olacak bir string yapıcısı
+        StringBuilder builder=new StringBuilder();
 
-        byte [] buffer=new byte[1024];//Veriler byte cinsi üzerinden olacağı için(ham veri almak(garanti olsun diye)) byte dizisi
+        //Veriler byte cinsi üzerinden olacağı için(ham veri almak(garanti olsun diye)) byte dizisi
+        byte [] buffer=new byte[1024];
         int i=0;
-        while ((i=inputStream.read(buffer))!=-1)//Okumayı byte dizisi üzerinden yapacağız
+        //Okumayı byte dizisi üzerinden yapacağız String yapıscısı ile byte dizisini stringe çeviriyoruz./Dosyaya içeriği yazdırıyoruz
+        while ((i=inputStream.read(buffer))!=-1)
         {
-            builder.append(Arrays.copyOfRange(buffer,0,i));//String yapıscısı ile byte dizisini stringe çeviriyoruz.
-            fileOutputStream.write(Arrays.copyOfRange(buffer,0,i));//Dosyaya içeriği yazdırıyoruz
+            builder.append(Arrays.copyOfRange(buffer,0,i));
+            fileOutputStream.write(Arrays.copyOfRange(buffer,0,i));
         }
         builder.toString();//İçeriği konsol'a yazdırıyoruz..
 
